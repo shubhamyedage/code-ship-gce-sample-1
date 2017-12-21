573337235125d8dd5693cb275fcb7907ab4c995c
shubham.yedage <shubham.yedage@synerzip.com>
2017-12-21 12:58:36 +0530

    Initital

---
+++ google-deploy.sh
@@ -0,0 +1,13 @@
+# Authenticate with the Google Services
+codeship_google authenticate
+
+# switch to the directory containing your app.yml (or similar) configuration file
+# note that your repository is mounted as a volume to the /deploy directory
+cd /deploy/
+
+apt-get install python-pip
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# deploy the application
+gcloud app deploy --quiet
