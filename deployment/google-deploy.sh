84813f74fbf647744ced43f6171bfcf2d660d196
shubham.yedage <shubham.yedage@synerzip.com>
2017-12-22 20:23:19 +0530

    Updated google-deploy.sh 14

--- deployment/google-deploy.sh
+++ deployment/google-deploy.sh
@@ -8,15 +8,16 @@ codeship_google authenticate
 # switch to the directory containing your app.yml (or similar) configuration file
 # note that your repository is mounted as a volume to the /deploy directory
 cd /deploy/main
-python --version
-apt-get -y update
-apt-get -y install python-pip python-dev build-essential
-pip install --upgrade pip
-pip install --upgrade pip
-pip install -r requirements.txt
+#python --version
+#apt-get -y update
+#apt-get -y install python-pip python-dev build-essential
+#pip install --upgrade pip
+#pip install --upgrade pip
+#pip install -r requirements.txt
 
 # deploy the application
 echo "Setting project"
 gcloud config set project text-summary-python27
+gsutil cp requirements.txt gs://text-summary-python27.appspot.com
 echo "Deploying"
 gcloud app deploy --bucket=gs://text-summary-python27.appspot.com --log-http --verbosity=debug --quiet
