f9ac63d9b11bba9022498d85dd89c24d0300182d
shubham.yedage <shubham.yedage@synerzip.com>
2017-12-22 18:15:02 +0530

    Updated google-deploy.sh 2 and deleted Dockerfile

--- deployment/google-deploy.sh
+++ deployment/google-deploy.sh
@@ -5,15 +5,14 @@ set -e
 date=$(date "+%Y%m%d%H%M")
 echo "${date}Authenticating"
 codeship_google authenticate
-ls
 
 # switch to the directory containing your app.yml (or similar) configuration file
 # note that your repository is mounted as a volume to the /deploy directory
-#cd /deploy/
+cd /deploy/
 
-#apt-get install python-pip
-#pip install --upgrade pip
-#pip install -r requirements.txt
+apt-get install python-pip
+pip install --upgrade pip
+pip install -r requirements.txt
 
 # deploy the application
 #gcloud app deploy --quiet
