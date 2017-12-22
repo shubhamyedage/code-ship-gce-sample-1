255497e5b42b8518ab4e8cb56195d13f20419400
shubham.yedage <shubham.yedage@synerzip.com>
2017-12-22 15:12:34 +0530

    Update configuration

--- deploy/google-deploy.sh
+++ deploy/google-deploy.sh
@@ -1,13 +1,17 @@
+echo "Authenticating"
+ls
+pwd
+
 # Authenticate with the Google Services
 codeship_google authenticate
 
 # switch to the directory containing your app.yml (or similar) configuration file
 # note that your repository is mounted as a volume to the /deploy directory
-cd /deploy/
+#cd /deploy/
 
-apt-get install python-pip
-pip install --upgrade pip
-pip install -r requirements.txt
+#apt-get install python-pip
+#pip install --upgrade pip
+#pip install -r requirements.txt
 
 # deploy the application
-gcloud app deploy --quiet
+#gcloud app deploy --quiet
