f92a761c5c5c96e15a688ea9359d30c356f67893
shubham.yedage <shubham.yedage@synerzip.com>
2017-12-22 18:51:06 +0530

    Updated google-deploy.sh 9 and added configration cached to codeship-services.yml

--- deployment/google-deploy.sh
+++ deployment/google-deploy.sh
@@ -9,9 +9,7 @@ codeship_google authenticate
 # switch to the directory containing your app.yml (or similar) configuration file
 # note that your repository is mounted as a volume to the /deploy directory
 cd /deploy/main
-ls
 python --version
-
 apt-get -y update
 apt-get -y install python-pip python-dev build-essential
 pip install --upgrade pip
@@ -19,4 +17,4 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 # deploy the application
-#gcloud app deploy --quiet
+gcloud app deploy --quiet
